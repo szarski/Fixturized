0ad631aa34716714e7606981989f60531e48fee8
Jacek Szarski <jacek@applicake.com>
2011-06-24 01:42:30 +0200

    remove file-related methods from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,15 +9,6 @@ require 'fixturized'
 require 'rspec'
 require 'mocha'
 
-TEMP_DIR = File.join(File.dirname(__FILE__),'..','temp')
-TEMP_FIXTURE_DIR = File.join(TEMP_DIR,'fixtures')
-
 RSpec.configure do |config|
    config.mock_with :mocha
-end
-
-def remove_temp_dir
-  if Dir[TEMP_DIR]
-    FileUtils.rm_rf TEMP_DIR
-  end
 end
