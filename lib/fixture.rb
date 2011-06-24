9ad6f1bc6b8078d87474de5310e2e5df86c22fd7
Jacek Szarski <jacek@applicake.com>
2011-06-24 03:17:59 +0200

    implemented all Fixture methods with full specs

--- lib/fixture.rb
+++ lib/fixture.rb
@@ -1,3 +1,39 @@
+require 'spec_helper'
 class Fixturized::Fixture
+  attr_reader :filename, :content
 
+  def self.serialization_module
+    Marshal
+  end
+
+  def initialize(filename)
+    @filename = filename
+    @content = {}
+  end
+
+  def save
+    Fixturized::FileHandler.write "#{filename}.yml", dump
+  end
+
+  def self.find(filename)
+    fixture = self.new(filename)
+    fixture.load Fixturized::FileHandler.read("#{filename}.yml")
+    return fixture
+  end
+
+  def [](*args)
+    self.content.send :[], *args
+  end
+
+  def []=(*args)
+    self.content.send :[]=, *args
+  end
+
+  def dump
+    return self.class.serialization_module.dump(content)
+  end
+
+  def load(value)
+    @content = self.class.serialization_module.load(value)
+  end
 end
--- spec/modules/fixture_spec.rb
+++ spec/modules/fixture_spec.rb
@@ -1,3 +1,62 @@
 require 'spec_helper'
 describe Fixturized::Fixture do
+
+  it "should initialize given a name" do
+    Fixturized::Fixture.new("whatever")
+  end
+
+  it "should store the filename" do
+    Fixturized::Fixture.new("whatever").filename.should == "whatever"
+  end
+
+  describe ".serialization_module" do
+    it "should return Marshal by default" do
+      Fixturized::Fixture.serialization_module.should == Marshal
+    end
+  end
+
+  describe "#dump" do
+    it "should use the proper serialization module" do
+      dump_result, serialization_module = mock(), mock()
+      serialization_module.expects(:dump).with({:some_key => :some_value}).returns dump_result
+      Fixturized::Fixture.expects(:serialization_module).returns serialization_module
+      fixture = Fixturized::Fixture.new("whatever")
+      fixture[:some_key] = :some_value
+      fixture.dump.should == dump_result
+    end
+  end
+
+  describe "#load" do
+    it "should use the proper serialization module" do
+      load_result, serialization_module = mock(), mock()
+      serialization_module.expects(:load).with({:some_key => :some_value}).returns load_result
+      Fixturized::Fixture.expects(:serialization_module).returns serialization_module
+      fixture = Fixturized::Fixture.new('fname')
+      fixture.load({:some_key => :some_value})
+      fixture.content.should == load_result
+    end
+  end
+
+  it "should save using FileHandler, with a proper name and content" do
+    dump_result = mock()
+    fixture = Fixturized::Fixture.new('whatever')
+    fixture.expects(:dump).returns(dump_result)
+    Fixturized::FileHandler.expects(:write).with("whatever.yml", dump_result)
+    fixture.save
+  end
+
+  describe ".find" do
+    it "should set the file name and load from file" do
+      file_content = mock()
+      Fixturized::FileHandler.expects(:read).with("whatever.yml").returns(file_content)
+      Fixturized::Fixture.any_instance.expects(:load).with(file_content)
+      fixture = Fixturized::Fixture.find "whatever"
+      fixture.filename.should == "whatever"
+    end
+  end
+
+  it "should be empty by default" do
+    Fixturized::Fixture.new("whatever").content.should == {}
+  end
+
 end
