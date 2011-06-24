a83c3dc8217d140c25fa3955ece2e064fa7d5d5c
Jacek Szarski <jacek@applicake.com>
2011-06-24 02:43:03 +0200

    removed old FileHandler methods

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -34,33 +34,4 @@ module Fixturized::FileHandler
   end
 
   self.extend self
-=begin
-  def fixture_dir
-    "#{RAILS_ROOT}/fixturized"
-  end
-
-  def create_fixture_dir
-    if Dir[self.fixture_dir].empty?
-      FileUtils.mkdir_p self.fixture_dir
-    end
-  end
-
-  def write_fixture(filename, objects)
-    Fixturized.create_fixture_dir
-    return File.open(self.fixture_path(filename), 'w') {|f| YAML.dump objects, f}      
-  end
-
-  def load_fixture(filename)
-    return YAML.load_file(self.fixture_path(filename))
-  end
-
-  def fixture_exists?(filename)
-    return FileTest.exists?(self.fixture_path(filename))
-  end
-
-  def fixture_path(filename)
-    return File.join(self.fixture_dir, filename)
-  end
-
-=end
 end
